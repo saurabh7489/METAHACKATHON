e56a78e7d8cd9a78187f1b46b7390aca74565e3c
saurabh7489 <saurabhpandey7489@gmail.com>
2026-04-09 13:07:33 +0530

    initial commit

--- inference.py
+++ inference.py
@@ -1,2 +1,28 @@
-def predict(input_data):
-    return {"message": "Inference working"}
+from env import DisasterEnv
+
+# Initialize environment
+env = DisasterEnv()
+
+def reset():
+    """
+    Reset the environment and return initial state.
+    """
+    return env.reset()
+
+def step(action):
+    """
+    Take an action and return result in OpenEnv format:
+    {
+        "state": ...,
+        "reward": ...,
+        "done": ...,
+        "info": {}
+    }
+    """
+    state, reward, done, info = env.step(action)
+    return {
+        "state": state,
+        "reward": reward,
+        "done": done,
+        "info": info
+    }
